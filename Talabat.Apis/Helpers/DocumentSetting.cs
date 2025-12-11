ec58bdc66bacbef0d2b4e69eb01aa656a7db9837
ahmed-thabet0100 <ahmed.thabetxx@gmail.com>
2025-12-11 11:13:24 +0200

    Add files via upload

---
+++ Talabat.Apis/Helpers/DocumentSetting.cs
@@ -0,0 +1,29 @@
+﻿namespace Talabat.APIs.Helpers
+{
+    public class DocumentSetting
+    {
+        public static string UploadFile(IFormFile File , string FolderName)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot//images", FolderName/*Products*/);
+
+            var fileName = $"{Guid.NewGuid()}{File.FileName}";
+
+            var filePath = Path.Combine(folderPath, fileName);
+
+            //4. save file as stream [data per time]
+            using var filestream = new FileStream(filePath, FileMode.Create);
+
+            File.CopyTo(filestream);
+
+            return $"/images/{FolderName}/{fileName}";
+
+        }
+
+        public static void DeleteFile(string FolderName , string FileName)
+        {
+            var filePath = Path.Combine( $"images", FolderName, FileName);
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+        }
+    }
+}
